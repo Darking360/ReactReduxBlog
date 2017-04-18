9339c47a1a512e3253dade085a70fdb85ace43b0
Miguel <MBolivar360@gmail.com>
2017-04-18 18:41:48 -0400

    Updating Boilerplate for React + Redux

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,45 +1,15 @@
 import React from 'react'
 import _ from 'lodash'
-import VideoList from './video_list'
-import SearchBar from './search_bar'
-import VideoDetail from './video_detail'
-import YTSearch from  'youtube-api-search'
-
-const API_KEY = 'AIzaSyDlRTrR5pS3m_-IbH4fv2mGR43FDmOPM10';
-
-
 
 class Main extends React.Component {
   constructor(props){
     super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: undefined
-    };
-
-    this.videoSearch("simple plan");
-
-  }
-
-  videoSearch(term){
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-      this.setState({
-        videos,
-        selectedVideo: videos[0]
-      });
-    });
   }
 
   render(){
-
-    const videoSearch = _.debounce(term => { this.videoSearch(term) },300);
-
     return(
       <div>
-        <SearchBar search={ videoSearch }/>
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} onVideoSelect={(selectedVideo) => this.setState({selectedVideo})} />
+        <h3>Boilerplate ready to go!</h3>
       </div>
     );
   }
